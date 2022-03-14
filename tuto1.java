78556f2103348d97af67bae148ebd8f773d1debf
Sanghoon Park <larchez@naver.com>
2022-03-14 18:37:48 +0900

    minusVal add on tuto1.java

--- tuto1.java
+++ tuto1.java
@@ -3,11 +3,16 @@ public class tuto1 {
   public static void main(String[] args) {
 
     System.out.println( addVal( 5, 6 ) );
+    System.out.println( minusVal( 5, 6 ) );
 
   }
 
   public static long addVal(long a, long b){
     return a + b;
+  }
+
+  public static long minusVal(long a, long b){
+    return a - b;
   }
 
 }
