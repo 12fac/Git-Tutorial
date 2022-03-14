51d99cbcbfe6082be46ecd7fc9ccf5e0ec80eb78
Sanghoon Park <larchez@naver.com>
2022-03-14 18:32:19 +0900

    add tuto1.java

---
+++ tuto1.java
@@ -0,0 +1,13 @@
+public class tuto1 {
+
+  public static void main(String[] args) {
+
+    System.out.println( addVal( 5, 6 ) );
+
+  }
+
+  public static long addVal(long a, long b){
+    return a + b;
+  }
+
+}
